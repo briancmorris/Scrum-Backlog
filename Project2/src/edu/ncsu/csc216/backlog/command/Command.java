c5b68155fbe7dbcfcbea78ab3489f20270bd2736
Walker Booth <walkbooth@gmail.com>
2017-03-21 17:54:56 -0400

    Implemented Command skeleton

--- Project2/src/edu/ncsu/csc216/backlog/command/Command.java
+++ Project2/src/edu/ncsu/csc216/backlog/command/Command.java
@@ -9,6 +9,41 @@ package edu.ncsu.csc216.backlog.command;
  */
 public class Command {
 
+	/** The details provided in a note */
+	private String note;
+	/** The author of the note */
+	private String noteAuthor;
+	
+	/** Contains the types of state that a task can have */
 	public static enum CommandValue { BACKLOG, CLAIM, PROCESS, VERIFY, COMPLETE, REJECT }
+	
+	
+	public Command(CommandValue c, String noteAuthor, String noteText) {
+		
+	}
+	
+	/**
+	 * Returns the command value 
+	 * @return commandValue the commandValue
+	 */
+	public CommandValue getCommand () {
+		return null;
+	}
+	
+	/**
+	 * Returns the note String
+	 * @return note the note string
+	 */
+	public String getNoteTest () {
+		return note;
+	}
+	
+	/**
+	 * Returns the note author String
+	 * @return noteAuthor the note author String
+	 */
+	public String getNoteAuthor () {
+		return noteAuthor;
+	}
 
 }
